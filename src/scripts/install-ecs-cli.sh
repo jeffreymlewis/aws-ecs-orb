e0d5b72df1b5a3c4d2485f40d3bd9533fe5afad5
Brian Vu <emailbrian.vu@gmail.com>
2022-05-26 13:59:19 -0700

    feat: added override-install parameter

--- src/scripts/install-ecs-cli.sh
+++ src/scripts/install-ecs-cli.sh
@@ -3,7 +3,7 @@ if [ $EUID == 0 ]; then export SUDO=""; else export SUDO="sudo"; fi
 # Platform check
 if uname -a | grep "Darwin"; then
     export SYS_ENV_PLATFORM="darwin"
-elif uname -a | grep "Linux"; then
+elif uname -a | grep "x86_64 GNU/Linux"; then
     export SYS_ENV_PLATFORM="linux"
 else
     echo "This platform appears to be unsupported."
@@ -11,11 +11,28 @@ else
     exit 1
 fi
 
-echo "https://amazon-ecs-cli.s3.amazonaws.com/ecs-cli-${SYS_ENV_PLATFORM}-amd64-${ECS_PARAM_VERSION}" >> test.txt
-echo "${ECS_PARAM_INSTALL_DIR}" >> test.txt
 
-$SUDO curl -Lo "${ECS_PARAM_INSTALL_DIR}" "https://amazon-ecs-cli.s3.amazonaws.com/ecs-cli-${SYS_ENV_PLATFORM}-amd64-${ECS_PARAM_VERSION}"
+Install_ECS_CLI(){
+    $SUDO curl -Lo "${ECS_PARAM_INSTALL_DIR}" "https://amazon-ecs-cli.s3.amazonaws.com/ecs-cli-${SYS_ENV_PLATFORM}-amd64-$1"
+    $SUDO chmod +x "${ECS_PARAM_INSTALL_DIR}"
+}
 
-$SUDO chmod +x "${ECS_PARAM_INSTALL_DIR}"
+Uninstall_ECS_CLI(){
+    $SUDO rm -rf "${ECS_PARAM_INSTALL_DIR}"
+}
 
-ecs-cli --version
+if [ "$(ecs-cli --version > /dev/null; echo $?)" -ne 0 ]; then
+    Uninstall_ECS_CLI
+    echo "Installing ECS CLI..."
+    Install_ECS_CLI "${ECS_PARAM_VERSION}"
+    ecs-cli --version
+else
+    if [ "$ECS_PARAM_OVERRIDE_INSTALLED" = 1 ]; then
+        echo "Overriding installed ECS CLI..."
+        Uninstall_ECS_CLI
+        Install_ECS_CLI "${ECS_PARAM_VERSION}"
+        ecs-cli --version
+    fi
+    echo "ECS CLI is already installed."
+fi
+
